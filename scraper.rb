6694b1045fbb1fa1ce787024a786a599e331c886
Cameron Petts <cameronpetts18@gmail.com>
2022-01-10 10:30:22 +0000

    updated comments

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,14 @@ require 'nokogiri'
 require 'byebug'
 require 'money'
 
+=begin
+    Two functions, one to retrieve a live conversion rate and another to look through CMC and collect CC information. 
+    main scraper function takes an integer as an argument for how many coins to search for.
+    returns an Array of coins with rank, name, price, marketcap, supply and other trading data.
+=end
+    
 def live_conversion_rate
+    # Reads xe.com for a more accurate price conversion
     I18n.enforce_available_locales = false
     url = "https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=GBP"
     unparsed_page = HTTParty.get(url)
@@ -16,7 +23,6 @@ def scraper
     # intialise url to search and array of coin data
     coin_array = Array.new()
     url = "https://coinmarketcap.com/"
-    limit = 10
     Money.add_rate("USD", "GBP", live_conversion_rate)
     
 
@@ -25,10 +31,13 @@ def scraper
     parsed_page = Nokogiri::HTML(unparsed_page)
 
     # searches for the tr elements inside tbody
-    live_conversion_rate
     rows = parsed_page.css('tbody tr')
+    
+    # searches through the td elements to fill the coin_array
+    limit = 10
     i = 0
     rows.each do |row|
+        # uses function argument to retrieve set amount of coins
         if i == limit
             return coin_array
         end
@@ -43,7 +52,6 @@ def scraper
             '24hrvolume': Money.from_amount(row.css('td')[7].css('p')[0].text.gsub(/,/, "").slice(1..-1).to_f, "USD").exchange_to("GBP").format,
             circulatingSupply: row.css('td')[8].text
         }
-        byebug
         # pushes coin data to output array
         coin_array << coin
         i = i + 1
